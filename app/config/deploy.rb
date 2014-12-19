82a0c71ed00abb87352c1597606aa7257f3c1c47
brunogoossens <brunogoossens@1198790.no-reply.drupal.org>
2014-12-19 11:30:49 +0100

    adding capifony deploy settings

---
+++ app/config/deploy.rb
@@ -0,0 +1,41 @@
+# deploy.rb
+
+set   :application,   "BBB Load Balancer"
+set   :deploy_to,     "/var/www/BBBLoadBalancer"
+set   :domain,        "localhost"
+
+set   :scm,           :git
+set   :repository,    "https://github.com/brunogoossens/BBB-Load-Balancer.git"
+
+role  :web,           domain
+role  :app,           domain, :primary => true
+role  :db,            domain, :primary => true
+
+set   :user,          "root"
+set   :use_sudo,      false
+set   :keep_releases, 5
+
+after "deploy", "deploy:cleanup"
+
+set :shared_files,      ["app/config/parameters.yml"]
+set :shared_children,     [app_path + "/logs", web_path + "/uploads", "vendor", "sessions"]
+
+set :use_composer, true
+set :update_vendors, false
+
+set :writable_dirs,       ["app/cache", "app/logs"]
+set :webserver_user,      "www-data"
+set :permission_method,   :acl
+set :use_set_permissions, true
+
+set :copy_vendors, false
+
+set :model_manager, "doctrine"
+
+set :dump_assetic_assets, true
+
+# namespace :deploy do
+#   task :restart do
+#     run "service php5-fpm restart"
+#   end
+# end
